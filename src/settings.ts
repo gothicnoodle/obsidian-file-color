787970fece2e74824bd716754efc4107c312f3b2
Jareth <cyberpunk13@gmail.com>
2026-03-28 17:29:34 -0700

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -1,5 +1,6 @@
 export type FileColorPluginSettings = {
   cascadeColors: boolean
+  cascadeToFiles: boolean
   colorBackground: boolean
   palette: Array<{
     id: string
@@ -10,11 +11,32 @@ export type FileColorPluginSettings = {
     path: string
     color: string
   }>
+  favorites: Array<{
+    id: string
+    value: string
+  }>
+  recentColors: Array<{
+    id: string
+    value: string
+  }>
 }
+
+export const BASIC_COLORS = [
+  { id: 'basic-red', name: 'Red', value: '#e05252' },
+  { id: 'basic-orange', name: 'Orange', value: '#e08c52' },
+  { id: 'basic-yellow', name: 'Yellow', value: '#e0d152' },
+  { id: 'basic-green', name: 'Green', value: '#52b052' },
+  { id: 'basic-blue', name: 'Blue', value: '#528be0' },
+  { id: 'basic-purple', name: 'Purple', value: '#9b52e0' },
+  { id: 'basic-grey', name: 'Grey', value: '#808080' },
+]
 
 export const defaultSettings: FileColorPluginSettings = {
   cascadeColors: false,
+  cascadeToFiles: false,
   colorBackground: false,
   palette: [],
   fileColors: [],
+  favorites: [],
+  recentColors: [],
 }
